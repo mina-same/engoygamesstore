c5b77ac46d0f4557eb6b4e059a815babd760ce14
mina samy lamhy <105872594+mina-same@users.noreply.github.com>
2024-12-12 01:34:45 +0200

    Update PrizeWheelClient.tsx
    reomve btn form PrizeWheelClient

--- components/PrizeWheelClient.tsx
+++ components/PrizeWheelClient.tsx
@@ -1,4 +1,4 @@
-  "use client";
+"use client";
 
 import React, { useState } from "react";
 import PrizeWheelPopup from "@/components/PrizeWheelPopup";
@@ -6,18 +6,10 @@ import PrizeWheelPopup from "@/components/PrizeWheelPopup";
 const PrizeWheelClient = () => {
   const [isWheelVisible, setWheelVisible] = useState(false);
 
-  const handleOpenWheel = () => setWheelVisible(true);
   const handleCloseWheel = () => setWheelVisible(false);
 
   return (
     <>
-      <button
-        onClick={handleOpenWheel}
-        className="mt-4 px-4 py-2 bg-blue-600 text-white rounded"
-      >
-        Open Prize Wheel
-      </button>
-
       <PrizeWheelPopup visible={isWheelVisible} onClose={handleCloseWheel} />
     </>
   );
